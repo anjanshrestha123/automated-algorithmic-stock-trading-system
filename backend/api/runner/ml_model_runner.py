aede8e49655472092a702fb1453956555aa9c36b
Anjan Shrestha <anjan.shrestha@gfs.com>
2023-04-23 19:28:31 -0400

    fixed bug

--- backend/api/runner/ml_model_runner.py
+++ backend/api/runner/ml_model_runner.py
@@ -3,4 +3,4 @@ from config import config
 
 
 def run_ml_model():
-    return os.system('python {model_path}/run.py'.format(model_path=config.get_model_path()))
+    return os.system('python3 {model_path}/run.py'.format(model_path=config.get_model_path()))
--- backend/api/runner/trading_system_runner.py
+++ backend/api/runner/trading_system_runner.py
@@ -3,4 +3,4 @@ from config import config
 
 
 def run_trading_system():
-    return os.system('python {trading_system_path}/run.py'.format(trading_system_path=config.get_trading_system_path()))
+    return os.system('python3 {trading_system_path}/run.py'.format(trading_system_path=config.get_trading_system_path()))
