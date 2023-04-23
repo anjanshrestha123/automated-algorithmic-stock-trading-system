2181cfcc3c9a54927178e756d9ed6cc6482b6fa6
Anjan Shrestha <anjan.shrestha@gfs.com>
2023-04-23 19:11:54 -0400

    added prod properties

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  stockTradingBackendUrl: 'http://localhost:5002/api/',
+  stockTradingBackendUrl: 'http://csce-5214-group-8.eastus.cloudapp.azure.com:5002/api/',
   production: true
 };
